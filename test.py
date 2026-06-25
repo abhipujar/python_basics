2425a3eff3baf79cf9623b080686038921942671
abhipujar <4al23ai037@aiet.org.in>
2026-06-25 16:54:31 +0530

    python_basic

---
+++ test.py
@@ -0,0 +1,33 @@
+# print("hello word","word",sep="#")
+# #print("hello word")
+# #print("hello word"
+# #
+# x=10.2
+# print(int(x))
+# y="10.5"
+# print(int(float(y)))
+# print("\t test\ntesting")
+# print(r"\test\ntesting")
+# print("\\test\\ntesting")
+# print("""hello
+#        my name is
+#        ram""")
+# x="hello"
+# print(x[0])
+# x="hello"
+# print(x[0:3:1]) #start:end:step
+# print(x[::-1]) #reverse string
+# x="hello"
+# print(x[0:1:2]) #start:end:step
+# print(x[::-1]) #reverse string
+
+
+# for i in range(1,10):
+#     if i%2==0:
+#         print(i)
+
+# for i in range(0,10,2):
+#     print(i)
+        
+# while True;
+#     print("hello")
